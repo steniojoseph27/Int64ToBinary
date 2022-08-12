74c5ca6dcc0b0cd2377ca0d540c9d5a17fe71630
Stenio Joseph <stenio.joseph.ext@encora.com>
2022-08-12 11:05:49 -0400

    Take a 64 bits going through them determine which one is on and which one is off

---
+++ Int64ToBinary/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Int64ToBinary
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LongToBinary.GetBinaryFromLong(457667858674766545);
+            Console.WriteLine();
+            LongToBinary.GetBinaryFromLong(234122343431232133);
+        }
+    }
+
+    public static class LongToBinary
+    {
+        public static void GetBinaryFromLong(long value)
+        {
+            long i;
+
+            Console.Write("0");
+            for (i = 1 << 30;  i > 0; i = i / 2)
+            {
+                if((value & i) != 0)
+                {
+                    Console.Write("1");
+                }
+                else
+                {
+                    Console.Write("0");
+                }
+            }
+        }
+    }
+}
